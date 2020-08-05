a3f1a6dde5e4e99f72c313475d86ac15619132d6
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-05 17:44:48 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'materialize-css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <button className='btn'>Button</button>
+    </div>
+  );
+}
+
+export default App;
