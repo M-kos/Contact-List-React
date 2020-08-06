d021d3cdcbcd0a1f2897dd51d27a08bdc103962b
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-06 16:59:15 +0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { useRoutes } from './routes'
+import { AuthContext } from './context/authContext'
+import { Http } from './services/http'
+import { login } from './utils/login'
+import { authRequestOptions } from './utils/authRequestOptions'
+
 import 'materialize-css'
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <button className='btn'>Button</button>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    isAuth: false,
+  }
+
+  componentDidMount() {
+    this.checkAuth()
+  }
+
+  checkAuth = async () => {
+    const user = login()
+
+    if (user && user.name && user.password) {
+      try {
+        const result = await Http.request(
+          authRequestOptions(user.name, user.password)
+        )
+
+        if (result) {
+          this.authHandler(!!result)
+        }
+        return
+      } catch (error) {}
+    }
+  }
+
+  authHandler = (value) => {
+    this.setState({
+      isAuth: value,
+    })
+  }
+
+  render() {
+    const { isAuth } = this.state
+    const routes = useRoutes(isAuth)
+
+    return (
+      <AuthContext.Provider
+        value={{
+          isAuth,
+          request: Http.request,
+          login,
+          authHandler: this.authHandler,
+        }}
+      >
+        <BrowserRouter>
+          <div className="App">{routes}</div>
+        </BrowserRouter>
+      </AuthContext.Provider>
+    )
+  }
 }
-
-export default App;
