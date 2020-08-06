969eb0ff1d582714cd474a711e002c4f1c2d1233
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-06 16:56:51 +0300

    Переделал сервер

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server')
+const bodyParser = require('body-parser')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -13,23 +14,40 @@ function isAuthorized(req, res, next) {
   }
   
   if (req.headers.authorization) {
-    const searchedUser = req.headers.authorization.split(' ')
+    const id = req.headers.authorization
     
-    const user = router.db.__wrapped__.users.filter(user => user.name === searchedUser[0] && user.password === searchedUser[1])
+    const user = router.db.__wrapped__.users.filter(user => user.id === id)
     
     if (user && user.length) {
       req.user = user[0]
       next()
-    } else {
-      res.sendStatus(401)
+    }
+  }
+
+  if (!req.headers.authorization && req.path === '/login' && req.method === 'POST') {
+    const {name, password} = req.body
+    const user = router.db.__wrapped__.users.filter(user => user.name === name && user.password === password)
+    
+    if (user && user.length) {
+      req.user = user[0]
+      next()
     }
   }
 }
 
-server.use(middlewares)
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
 server.use(isAuthorized)
 server.post('/login', (req, res) => {
-  res.status(200).json(req.user)
+  console.log('POST');
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.json(req.user)
+  // res.status(200).json(req.user)
+  console.log('AFTER1');
+})
+server.use((req, res) => {
+  console.log('AFTER2');
 })
 server.use(router)
 server.listen(3001, () => {
